78e74ea596f5f45d26edc258c6ff1872859501cd
Henrique Rocha <rochahenrique.1023@gmail.com>
2024-12-04 01:06:05 -0300

    Ajuste no CSS

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,7 @@
 import instagram from '../../assets/Hero/instagram.png'
-import relatorio from '../../assets/Hero/relatorio.png'
 import linkedin from '../../assets/Hero/linkedin.png'
-import usuario from '../../assets/Hero/usuario.png'
-import galeria from '../../assets/Hero/galeria.png'
-import GitHub from '../../assets/Hero/GitHub.png'
+import GitHub from '../../assets/Hero/gitHub.png'
 import gmail from '../../assets/Hero/gmail.png'
-import email from '../../assets/Hero/email.png'
 import LottiePerfil from '../lotties/LottiePerfil';
 import './NavBar.css'
 
@@ -21,24 +17,20 @@ const NavBar = () => {
         <a href='https://rochahenrique.1023@gmail.com' target='_blank' className='button-redes' ><img className='img-redes' src={gmail} alt="" /></a>
         <a href='https://instagram.com/henrique.rocha_/' target="_blank" className='button-redes' ><img className='img-redes' src={instagram} alt="" /></a>
       </div>
-
+ 
       <div className='container-links'>
-        <a href="#segundaDobra" className='container-navigation'>
-          <img src={usuario} alt="" />
-        <p>Sobre</p>
-        </a>
-        <a className='container-navigation'>
-          <img src={relatorio} alt="" />
-        <p>Skills</p>
-        </a>
-        <a className='container-navigation'>
-          <img src={galeria} alt="" />
-        <p>Portifólio</p>
-        </a>
-        <a className='container-navigation'>
-          <img src={email} alt="" />
-        <p>Contato</p>
-        </a>
+        <span className='container'>
+          <button className='button'>Sobre</button>
+        </span>
+        <span className='container'>
+          <button className='button'>Skills</button>
+        </span>
+        <span className='container'>
+          <button className='button'>Portifólio</button>
+        </span>
+        <span className='container'>
+          <button className='button'>Contato</button>
+        </span>
       </div>
     </div>
   );
