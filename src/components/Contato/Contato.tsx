fe158fd720537718f19db99f85df6b411b08c20e
Henrique Rocha <rochahenrique.1023@gmail.com>
2024-12-14 01:25:26 -0300

    Página de Contato finalizada

--- src/components/Contato/Contato.tsx
+++ src/components/Contato/Contato.tsx
@@ -3,8 +3,40 @@ import './Contato.css'
 const Contato = () => {
     return (
       <div className='container-contato' id='Contato'>
-        <h1 className="title-contato">Sobre</h1>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <div className='container-title-contato'>
+          <h1 className="title-contato">Contato</h1>
+          <p>Entre em contato para discutir projetos ou colaborações. Preencha o formulário abaixo e eu retornarei o mais breve possível. Estou ansioso para conectar com você!</p>
+        </div>
+
+        <div className='container-form'>
+          <div className='container-map'>
+            <button className='map-button'>Telefone</button>
+
+            <div className='map'>
+
+            </div>
+          </div>
+
+          <div className='form'>
+            <div className='form-group'>
+              <label>E-mail</label>
+              <input type='email' placeholder='Digite seu e-mail...' />
+            </div>
+
+            <div className='form-group'>
+              <label>Assunto</label>
+              <input type='text' placeholder='Digite o assunto...' />
+            </div>
+
+            <div className='form-group'>
+              <label>Mensagem</label>
+              <textarea rows={8} placeholder='Escreva sua mensagem...'></textarea>
+            </div>
+
+            <button className='form-button'>Enviar mensagem</button>
+          </div>
+        </div>
+      
       </div>
     )
 }
