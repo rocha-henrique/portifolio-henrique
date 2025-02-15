bc3b89b9919812ea05a3f27711f05d541315f71f
root <rochahenrique@gmail.com>
2025-02-15 19:20:27 -0300

    Ajuste na página sobre

--- src/components/Sobre/Sobre.tsx
+++ src/components/Sobre/Sobre.tsx
@@ -1,6 +1,7 @@
-import './Sobre.css';
 import henrique from '../../assets/Sobre/henrique.png'
 import StacksCarousel from './stack';
+import './SobreMobile.css';
+import './Sobre.css';
 
 const Sobre = () => {
   
@@ -11,18 +12,21 @@ const Sobre = () => {
       </div>
 
       <div className='container-perfil'>
-        
         <img className='img-perfil' src={henrique} alt="" />
         <div className='container-texto-perfil'>
           <h1 className='title-name'>Henrique Rocha</h1>
-          <p className='texto-perfil text'>
-            💻 Desenvolvedor Front-End com experiência em JavaScript, TypeScript, React e WordPress. 
-          </p>
+          
+          <div className='container-gap-perfil'>
+            <p className='texto-perfil text'>
+              💻 Desenvolvedor Front-End com experiência em JavaScript, TypeScript, React e WordPress. 
+            </p>
 
-          <p className='texto-perfil text'>
-            🚀 Criei landing pages personalizadas para clientes na agência Nova Dimensão Digital, além de projetos próprios, como sites e blogs interativos.
-          </p>
+            <p className='texto-perfil text'>
+              🚀 Criei landing pages personalizadas para clientes na agência Nova Dimensão Digital, além de projetos próprios, como sites e blogs interativos.
+            </p>
+          </div>
 
+        <div className='container-gap-perfil'>
           <p className='texto-perfil text'>
             🎯 Focado em soluções eficientes e responsivas, combinando design moderno e alta performance.
           </p>
@@ -30,6 +34,7 @@ const Sobre = () => {
           <p className='texto-perfil text'>
             🔍 Busco desafios que me permitam aplicar minha criatividade e habilidades técnicas para gerar impacto.
           </p>
+          </div>
         </div>
       </div>
     </div>
